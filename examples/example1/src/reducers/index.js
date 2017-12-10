d588bc0787b2f3e21d7335485963e3125802e5c7
Solaiman Kmail <s.kmail@blue.ps>
2017-12-11 01:48:42 +0200

    Add examples

--- examples/example1/src/reducers/index.js
+++ examples/example1/src/reducers/index.js
@@ -1,7 +1,11 @@
 import { combineReducers } from 'redux';
+import customers from './customers';
+import auth from './auth';
 // ##REDUCER_IMPORT##
 
 const rootReducer = combineReducers({
+  customers,
+  auth,
   // ##REDUCER_KEY##
 });
 
